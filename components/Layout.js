0ceef92646dfbbd9d301f78360576969e4bd075f
“[Nicholas <n.samaras4@outlook.com>
2022-12-27 16:13:51 -0500

    Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
 import styles from '../styles/Layout.module.css'
 
 export default function Layout({ title, keywords, description, children }) {
@@ -11,9 +13,11 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name='keywords' content={keywords} /> 
       </Head>
 
+    <Header />
     <div className={styles.container}>
       {children}
     </div>
+    <Footer />
     </div>
   )
 }
