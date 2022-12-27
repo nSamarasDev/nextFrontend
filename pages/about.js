84b365af6b1667879db84abd96a0342c8a916ada
“[Nicholas <n.samaras4@outlook.com>
2022-12-27 14:58:52 -0500

    about.js update

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title='About DJ Events'>
       <h1>About</h1>
       <p>This is an app to find the latest Dj or other musical events</p>
       <p>Version: 1.0.0</p>
-    </div>
+    </Layout>
   )
 }
 
