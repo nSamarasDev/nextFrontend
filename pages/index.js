18486f09c4f09807e522f41852f9701202a417cf
“[Nicholas <n.samaras4@outlook.com>
2022-12-27 14:58:23 -0500

    index.js home update

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-import Link from 'next/link'
-
+import React from 'react'
+import Layout from '../components/Layout'
 
 
 
 export default function HomePage() {
   return (
-    <>
-    <div>
+    <Layout>
     <h1>Home</h1>
-    </div>
-    </>
+    </Layout>
+
   )
 }
