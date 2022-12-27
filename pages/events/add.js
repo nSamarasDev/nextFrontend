9371581893861d6683e609d712bb1cf2fb9aa756
“[Nicholas <n.samaras4@outlook.com>
2022-12-27 14:59:59 -0500

    add.js update

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Layout from '../../components/Layout'
 
 export default function AddEventPage() {
   return (
-    <div>
+    <Layout title='Add New Event'>
       <h1>Add Events</h1>
-    </div>
+    </Layout>
   )
 }
