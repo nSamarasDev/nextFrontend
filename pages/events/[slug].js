7f911887d27335078797a842972c5dfbdd0508e8
“[Nicholas <n.samaras4@outlook.com>
2022-12-27 13:16:43 -0500

    [0]slug.js events

---
+++ pages/events/[slug].js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+export default function EventPage() {
+  const router = useRouter()
+
+  console.log(router)
+
+  return (
+    <div>
+      <h1>My Event</h1>
+      <h3>{router.query.slug}</h3>
+    </div>
+  )
+}
