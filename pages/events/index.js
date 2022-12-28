3a0d6a637579a58473ac926ae26837cd4ec22378
“[Nicholas <n.samaras4@outlook.com>
2022-12-28 11:15:43 -0500

    index.js events update

--- pages/events/index.js
+++ pages/events/index.js
@@ -4,7 +4,9 @@ import Layout from '../../components/Layout'
 export default function EventsPage() {
   return (
     <div>
+      <Layout>
       <h1>My Events</h1>
+      </Layout>
     </div>
   )
 }
