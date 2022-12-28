2b7661f4f019288dadace5513363bea35992041e
“[Nicholas <n.samaras4@outlook.com>
2022-12-28 12:11:14 -0500

    404 page added

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaExclamationTriangle } from 'react-icons/fa'
+import Layout from '../components/Layout'
+import styles from '../styles/404.module.css'
+import Link from 'next/link'
+
+export default function NotFoundPage() {
+  return (
+    <Layout title='Page Not Found'>
+     <div className={styles.error}>
+        <h1><FaExclamationTriangle />  404</h1>
+        <h1>Sorry, there is nothing here</h1>
+        <Link href='/'>Go Back</Link>
+     </div>
+    </Layout>
+  )
+}
