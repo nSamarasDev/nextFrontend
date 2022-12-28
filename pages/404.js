9d2d50fdafa096b11be5057352880431ac03879e
“[Nicholas <n.samaras4@outlook.com>
2022-12-28 12:37:17 -0500

    404.js update

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaExclamationTriangle } from 'react-icons/fa'
-import Layout from '../components/Layout'
-import styles from '../styles/404.module.css'
+import Layout from '@/components/Layout'
+import styles from '@/styles/404.module.css'
 import Link from 'next/link'
 
 export default function NotFoundPage() {
