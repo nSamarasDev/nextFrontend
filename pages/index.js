536e89a3b5c8d3da12a5728e8154b65449b013df
“[Nicholas <n.samaras4@outlook.com>
2022-12-29 13:28:30 -0500

    index.js page update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
 
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  console.log(events)
   return (
     <Layout>
-    <h1>Home</h1>
+    <h1>Upcomming Events</h1>
     </Layout>
 
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`) 
+  const events = await res.json()
+
+  return {
+    props: {events},
+    revalidate: 1,
+  }
 }
