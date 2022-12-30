08437601f5e406d690107475a7aaf92b8217daee
“[Nicholas <n.samaras4@outlook.com>
2022-12-30 15:43:21 -0500

    index.js home updated

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Layout from '@/components/Layout'
+import Link from 'next/link'
+import EventItem from '@/components/EventItem'
 import { API_URL } from '@/config/index'
 
 
@@ -9,6 +11,16 @@ export default function HomePage({ events }) {
   return (
     <Layout>
     <h1>Upcomming Events</h1>
+    {events.length === 0 && <h3>No events to show</h3>}
+    {events.map((evt) => (
+      <EventItem key={evt.id} evt={evt} />
+    ))}
+
+    {events.length > 0 && (
+      <Link legacyBehavior href='/events'>
+        <a className='btn-secondary'>View all events</a>
+      </Link>
+    )}
     </Layout>
 
   )
@@ -19,7 +31,7 @@ export async function getStaticProps() {
   const events = await res.json()
 
   return {
-    props: {events},
+    props: {events:events.slice(0, 3)},
     revalidate: 1,
   }
 }
