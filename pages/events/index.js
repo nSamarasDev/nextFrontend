ed1ccea51a16f80e47c64fd22f69e620b5d301c9
“[Nicholas <n.samaras4@outlook.com>
2022-12-30 15:43:54 -0500

    index.js events page updated

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
 
-export default function EventsPage() {
+
+
+export default function EventsPage({ events }) {
+  console.log(events)
   return (
-    <div>
-      <Layout>
-      <h1>My Events</h1>
-      </Layout>
-    </div>
+    <Layout>
+    <h1>Events</h1>
+    {events.length === 0 && <h3>No events to show</h3>}
+    {events.map((evt) => (
+      <EventItem key={evt.id} evt={evt} />
+    ))}
+    </Layout>
+
   )
 }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`) 
+  const events = await res.json()
+
+  return {
+    props: {events},
+    revalidate: 1,
+  }
+}
+
