15bde453c44536d4a564393d29d647fa14d2cd19
“[Nicholas <n.samaras4@outlook.com>
2022-12-31 13:12:32 -0500

    update

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,16 +1,56 @@
 import React from 'react'
 import Layout from '@/components/Layout'
 import { useRouter } from 'next/router'
+import {API_URL} from '@/config/index'
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   const router = useRouter()
 
   console.log(router)
 
   return (
     <Layout>
-      <h1>My Event</h1>
-      <h3>{router.query.slug}</h3>
+      <h1>{evt.name}</h1>
     </Layout>
   )
 }
+
+export async function getStaticPaths() {
+ const res = await fetch(`${API_URL}/api/events/`)
+ const events = await res.json()
+
+ const paths = events.map(evt => ({
+  params: {slug: evt.slug}
+ }))
+
+  return {
+    paths,
+    fallback: false 
+  }
+
+}
+
+export async function getStaticProps({params:{slug}}) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+
+  const events = await res.json()
+  
+  return {
+    props: {
+      evt: events[0]
+    },
+    revalidate: 1,
+  }
+}
+
+// export async function getServerSideProps({query:{slug}}) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+
+//   const events = await res.json()
+  
+//   return {
+//     props: {
+//       evt: events[0]
+//     }
+//   }
+// }
