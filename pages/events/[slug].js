97c68c88ac03458015786b0c80cba4b72ed72f37
“[Nicholas <n.samaras4@outlook.com>
2022-12-31 13:49:55 -0500

    slug page update

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,16 +1,54 @@
 import React from 'react'
+import {FaPencilAlt, FaTimes} from 'react-icons/fa'
+import Link from 'next/link'
+import Image from 'next/image'
 import Layout from '@/components/Layout'
-import { useRouter } from 'next/router'
+import styles from '@/styles/Event.module.css'
 import {API_URL} from '@/config/index'
 
 export default function EventPage({ evt }) {
-  const router = useRouter()
-
-  console.log(router)
+  const deleteEvent = (e) => {
+    console.log('delete')
+  }
+  
 
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link legacyBehavior href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event 
+            </a>  
+          </Link>
+           <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+           </a>
+        </div>
+
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venu: {evt.venu}</h3>
+        <p>{evt.address}</p>
+
+        <Link legacyBehavior href='/events'>
+          <a className={styles.back}>
+           {'<'}Go Back
+          </a>
+        </Link>
+      </div>
     </Layout>
   )
 }
